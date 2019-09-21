a5a83c7920c6dff7bf3dd5c55b69a9936d23847c
Rafael <rafa@adada.com>
2019-09-20 22:27:17 -0300

    atualizando repositório

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import * as Typed from 'typed.js';
 @Component({
   selector: 'has-content',
   templateUrl: './content.component.html',
@@ -10,6 +10,20 @@ export class ContentComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    const options = {
+      stringsElement: '#typed-strings',
+      strings: ['PAPAI NOEL', 'NÃO', 'EXISTEEEEEEEEEEE'],
+      typeSpeed: 100,
+      backSpeed: 100,
+      backDelay: 200,
+      smartBackspace: true,
+      fadeOut: true,
+      showCursor: false,
+      startDelay: 1000,
+      loop: true
+    };
+	
+	const typed = new Typed('.typing-element', options);
   }
 
 }
